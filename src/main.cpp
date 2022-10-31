24ea7281ef157f3f4d289e48801f6f39c9ee0477
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-10-30 21:52:34 -0300

    impl update

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,28 @@
 
 using namespace std;
 
+static int _id = 0;
+
 class Product
 {
 public:
+    int Id;
     string Name;
     double Price;
 
+    Product( )
+    {
+
+    }
+
     Product(string name, double price)
+    {
+        Id = _id++;
+        Name = name;
+        Price = price;
+    }
+
+    void Update(string name, double price) 
     {
         Name = name;
         Price = price;
@@ -23,7 +38,7 @@ int main()
     int productsToCreate = 0;
     int option = 0;
 
-    cout << "\nWelcome! What do you want to do?";
+    cout << "\nWelcome! What do you want to do?" << endl;
     string options("");
     options.append("\n1 - Create");
     options.append("\n2 - Read");
@@ -49,34 +64,87 @@ int main()
 
                 products.push_back(Product(name, price));
 
+                cout << "created!" << endl;
+
                 break;
             }
             case 2:
+            {
                 if (products.size() == 0)
                 {
-                    cout << "\nno data\n";
+                    cout << "\nno data\n" << endl;
                     break;
                 }
 
-                cout << "\nProducts:\n";
-                for (int i = 0; i < products.size(); i++)
+                string list("\nProducts:");
+                for(Product product : products)
                 {
-                    cout << products[i].Name + ", " << products[i].Price << "$\n";
+                    list.append("\nid: " + to_string(product.Id) + " | name: " + product.Name + " | price: " + to_string(product.Price));
                 }
 
+                cout << list << endl;
+
                 break;
+            }
+
+            case 3:
+            {
+                int id = 0;
+
+                cout << "Product Id: ";
+                cin >> id;
+
+                Product productToUpdate;
+                int productToUpdateIndex;
+
+                for (int i = 0; i < products.size(); i++)
+                {
+                    if (products[i].Id == id)
+                    {
+                        productToUpdate = products[i];
+                        productToUpdateIndex = i;
+                        break;
+                    }
+                }
+
+                if (productToUpdate.Id != id)
+                {
+                    cout << "not found" << endl;
+                    break;
+                }
+
+                cout << "\nid: " + to_string(productToUpdate.Id) + " | name: " + productToUpdate.Name + " | price: " + to_string(productToUpdate.Price) + "\n" << endl;
+
+                string name = "";
+                double price = 0.0;
+
+                cout << "\nNew product name: ";
+                cin >> name;
+                cout << "New product price: ";
+                cin >> price;
+
+                productToUpdate.Update(name, price);
+
+                products[productToUpdateIndex] = productToUpdate;
+
+                cout << "updated!" << endl;
+
+                break;
+            }
 
             default:
-                cout << "\ninvalid option\n";
+            {
+                cout << "\ninvalid option\n" << endl;
                 cout << options;
                 cin >> option;
+            }
         }
 
         cout << options;
         cin >> option;
     }
 
-    cout << "\nsee you later!";
+    cout << "\nsee you later!" << endl;
 
     system("pause>0");
 }
